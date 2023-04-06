2217af20ab54838f2142a4da7bb4b7ae514832a7
TestSeung <sjlim589@hanmail.net>
2023-04-06 15:28:22 +0900

    상속

---
+++ Character.cpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "Character.h"
+#include <iostream>
+
+void Character::Move()
+{
+	std :: cout << "이동한다" << "\n";
+}
+
+void Character::Atteck()
+{
+}
---
+++ Character.h
@@ -0,0 +1,13 @@
+#pragma once
+class Character
+{
+public:
+	//property,member variable
+	int MP;
+	int HP;
+
+	//method,member fuction
+	void Move();
+	void Atteck();
+};
+
